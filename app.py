de082f561b979aafa563f10eb1b7b8c220da7ca6
JRose31 <jroseborough31@gmail.com>
2022-12-26 03:17:02 -0800

    Updates to image-upload & data processing

--- app.py
+++ app.py
@@ -13,17 +13,29 @@ def test():  # put application's code here
     return render_template('test.html', name='Test')
 
 
-@app.route('/image-upload', methods=('GET', 'POST'))
+@app.route('/image-upload', methods=['GET', 'POST'])
 def img_upload():
+    data = {"Pet types": ['Dog', 'Cat', 'Ferret', 'Snake', 'Bird']}
+    return render_template('image_upload.html', name='Uploader', data=data)
+
+
+@app.route('/image-processing', methods=['POST'])
+def img_processing():
     if request.method == 'POST':
-        petname = request.form['petname']
-        pettype = request.form.get('pettype')
-        img_obj = request.files.get('imagefile')
+        pet_name = request.form['pet_name']
+        pet_type = request.form.get('pet_type')
+        img_obj = request.files.get('image_file')
         img_file = img_obj.filename
 
-        print(f"All values passed: {petname, pettype, img_file}")
+        pet_data = {
+            'name': pet_name,
+            'type': pet_type,
+            'filename': img_file
+        }
 
-    return render_template('image_upload.html', name='Uploader')
+        print(f"All values passed: {pet_name, pet_type, img_file}")
+
+    return render_template('image_processing.html', name='Processor', data=pet_data)
 
 
 if __name__ == '__main__':
