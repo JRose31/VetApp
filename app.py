cdbe58ed11d2c242f71716116f5dd6b8887d3cf7
JRose31 <jroseborough31@gmail.com>
2022-12-25 17:32:02 -0800

    Added image-upload page & view

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,19 @@ def hello_world():  # put application's code here
 @app.route('/test')
 def test():  # put application's code here
     return render_template('test.html', name='Test')
+
+
+@app.route('/image-upload', methods=('GET', 'POST'))
+def img_upload():
+    if request.method == 'POST':
+        petname = request.form['petname']
+        pettype = request.form.get('pettype')
+        img_obj = request.files.get('imagefile')
+        img_file = img_obj.filename
+
+        print(f"All values passed: {petname, pettype, img_file}")
+
+    return render_template('image_upload.html', name='Uploader')
 
 
 if __name__ == '__main__':
